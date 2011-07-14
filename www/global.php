3f2bba1d385d68f7f789a0e3f8025fc73a478403
Nick Mitin <nick.mitin@gmail.com>
2011-07-14 12:30:36 +0400

    application fix

--- www/global.php
+++ www/global.php
@@ -3,34 +3,33 @@
 ini_set('error_reporting', E_ALL);
 error_reporting(E_ALL);
 
-function __autoload($className)
-{ 
-  if (file_exists(projectRoot . '/controllers/' . $className . '.php')) 
+function pawAutoload($className)
+{
+  if (file_exists(projectRoot . '/controllers/' . $className . '.php'))
   {
     require_once(projectRoot . '/controllers/' . $className . '.php');
-  }
-  else
-  {    
-    if (!include(projectRoot . '/lib/' . $className . '.php'))
-    {
-      throw new Exception();
-    }
+  } 
+  else if (file_exists(projectRoot . '/lib/' . $className . '.php'))
+  {
+    require_once(projectRoot . '/lib/' . $className . '.php');
   }
 }
+
+spl_autoload_register('pawAutoload', true);  
 
 mb_internal_encoding('UTF-8');
 mb_regex_encoding('UTF-8');
 
 if (!isset($_SERVER['HTTP_USER_AGENT']))
 {
-	$_SERVER['HTTP_USER_AGENT'] = 'N/A';
+  $_SERVER['HTTP_USER_AGENT'] = 'N/A';
 }
                                     
 header('cache-control: no-cache', true);
 header('content-type: text/html; charset=utf-8', true);
 
 require($_SERVER['DOCUMENT_ROOT'] . '/../conf/application.conf');
-																	
-$application = new bmFuckFrameworks(null);
+
+$application = new bmApplication(null);
 
 ?>
